6867b7118d0c11581052f0c4f12ea2413fa11f53
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-02-24 14:31:42 -0300

    Format chart data to be plotted

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Line } from "react-chartjs-2";
+import defaultOptions from './defaultOptions';
 
-const Chart = ({ data, options, ...rest }) => {
-  return <Line data={data} options={options} {...rest} />
+const Chart = ({ data, customOptions, ...rest }) => {
+  const options = { ...defaultOptions, ...customOptions }
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [{ data: [] }]
+  })
+
+  const formatData = data => {
+    return data.map(item => ({
+      t: new Date(item[0]),
+      y: item[1]
+    }))
+  }
+
+  const formatLabel = data => {
+    return data.map(item => new Date(item[0]))
+  }
+
+  useEffect(() => {
+    const formattedData = formatData(data)
+    const formattedLabel = formatLabel(data)
+
+    setChartData({
+      labels: formattedLabel,
+      datasets: [{ data: formattedData }]
+    })
+  }, [data])
+
+  return <Line data={chartData} options={options} {...rest} />
 }
 
 export default Chart
