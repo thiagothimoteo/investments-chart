ebfe9acba5bb38ba4c8547d538a4c51b304e4204
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-02-24 09:39:17 -0300

    Add styled-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 960px;
+  margin: 0 auto;
+`
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       Hello World
-    </div>
+    </Container>
   );
 }
 
