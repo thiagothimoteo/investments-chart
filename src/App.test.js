a46fdbfd6b3b2b094244fbc76668185c01daa9c1
Thiago Thimóteo <thiago.thimoteo@gmail.com>
2020-02-25 21:35:22 -0300

    Add spec to App

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, waitForElement } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('#App', () => {
+  it('renders correctly', async () => {
+    const { container, getByRole, getByText } = render(<App />);
+    const select = container.querySelector('select')
+    const description = getByRole('heading')
+    const blankslate = getByText('Não há dados a serem mostrados')
+
+    expect(select.value).toBe('all_time')
+    expect(description).toBeInTheDocument()
+    expect(blankslate).toBeInTheDocument()
+
+    const chart = await waitForElement(() => container.querySelector('canvas'))
+
+    expect(chart).toBeInTheDocument()
+    expect(blankslate).not.toBeInTheDocument()
+  })
+})
